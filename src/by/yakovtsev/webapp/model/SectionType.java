0e1136863c3e6f2a23fccf74da81245e5fe84ed3
An4or <anchor_gomel@tut.by>
2019-05-26 19:00:44 +0300

    added packages model and storage and added them in the main classes

---
+++ src/by/yakovtsev/webapp/model/SectionType.java
@@ -0,0 +1,22 @@
+package by.yakovtsev.webapp.model;
+
+
+public enum SectionType {
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPERIENCE("Опыт работы"),
+    EDUCATION("Образование");
+
+    private String title;
+
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+}
---
+++ src/by/yakovtsev/webapp/storage/IStorage.java
@@ -0,0 +1,24 @@
+package by.yakovtsev.webapp.storage;
+
+import by.yakovtsev.webapp.model.Resume;
+
+import java.util.Collection;
+
+
+public interface IStorage {
+    void clear();
+
+    void save(Resume r) throws Exception; //add WebAppException
+
+    void update(Resume r);
+
+    Resume load(String uuid);
+
+    void delete(String uuid);
+
+    Collection<Resume> getAllSorted();
+
+    int size();
+
+    boolean isSectionSupported();
+}
